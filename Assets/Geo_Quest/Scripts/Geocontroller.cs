616b32dc6f7afec309ff6d074e36e2b076b41df5
meowl-me <sareakilah@gmail.com>
2025-09-27 12:20:52 -0400

    9/27/25 CW

---
+++ Assets/Geo_Quest/Scripts/Geocontroller.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Geocontroller : MonoBehaviour
+{
+    string Hello = ("Hello");
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello World!");
+        string World = ("world");
+        Debug.Log(Hello + World);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
