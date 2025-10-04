e31c3a81ddd3e7e0c665dd1d6f8de22d05b8761e
meowl-me <sareakilah@gmail.com>
2025-10-04 12:25:17 -0400

    10/04/25 CW
    C++ scripts I did in class. (B4 Lunch time)

--- Assets/Geo_Quest/Scripts/Geocontroller.cs
+++ Assets/Geo_Quest/Scripts/Geocontroller.cs
@@ -1,24 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
 
 
 
-public class Geocontroller : MonoBehaviour
+public class GeoController: MonoBehaviour
 {
-    string Hello = ("Hello");
+    /*
+    //string Hello = ("Hello");
+    */
+    private int varOne = 3;
+    public float speed = 5;
+    private Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello World!");
-        string World = ("world");
-        Debug.Log(Hello + World);
+        /*
+        // Debug.Log("Hello World!");
+        // string World = ("world");
+        // Debug.Log(Hello + World);
+        */
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(varOne++);
+        /*
+        //rb.velocity = new Vector2(-1, rb.velocity.x);
+        */
+        float xInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2 (xInput * speed, rb.velocity.y);
+        Debug.Log(xInput);
     }
+
+    private void()
+                
 }
